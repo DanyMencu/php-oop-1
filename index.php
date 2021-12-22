443d8bd49ca46002bd031b6fdf13b705d6febde3
DanyMencu <dany69men@gmail.com>
2021-12-22 16:45:16 +0100

    Exercise completed, without css

--- index.php
+++ index.php
@@ -1,16 +1,169 @@
-<?php 
+<?php
 //Import data movies
+require_once __DIR__ . '/script/database_movie.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Poracciflix PHP</title>
 </head>
+
 <body>
-    
+    <main>
+        <div class="container">
+            <!-- Avengers End Game -->
+            <div class="movie-card">
+                <!-- Postert -->
+                <div class="poster">
+                    <img src="<?php echo $avengers_endgame->poster ?>" alt="<?php echo $avengers_endgame->title ?>">
+                </div>
+                <ul>
+                    <li class="title">
+                        <?php echo $avengers_endgame->title ?>
+                    </li>
+                    <li> <strong>Genere:</strong>
+                        <?php echo $avengers_endgame->genre ?>
+                        <strong>Anno di uscita:</strong>
+                        <?php echo $avengers_endgame->exit_year ?>
+                    </li>
+                    <li><strong>Lingua originale:</strong>
+                        <?php echo $avengers_endgame->original_lenguage ?> , Durata: <?php echo $avengers_endgame->duration ?> minuti
+                    </li>
+                    <li><strong>Voto:</strong>
+                        <?php echo $avengers_endgame->votes ?>
+                        / <strong>Consigliato?</strong>
+                        <?php echo $avengers_endgame->getRecommendation($avengers_endgame->votes) ?>
+                    </li>
+                    <li><strong>Costo:</strong>
+                        <?php echo $avengers_endgame->production_cost ?> milioni
+                    </li>
+                    <li><strong>Incasso:</strong>
+                        <?php echo $avengers_endgame->tickets_sold ?> milioni
+                    </li>
+                    <li>
+                        <strong>Ci sarà un sequel?</strong>
+                        <?php echo $avengers_endgame->getSequel($avengers_endgame->production_cost, $avengers_endgame->tickets_sold) ?>
+                    </li>
+                </ul>
+            </div>
+            <!-- Dune -->
+            <div class="movie-card">
+                <!-- Postert -->
+                <div class="poster">
+                    <img src="<?php echo $dune->poster ?>" alt="<?php echo $dune->title ?>">
+                </div>
+                <ul>
+                    <li class="title">
+                        <?php echo $dune->title ?>
+                    </li>
+                    <li>
+                        <strong>Genere:</strong>
+                        <?php echo $dune->genre ?>
+                        <strong>Anno di uscita:</strong>
+                        <?php echo $dune->exit_year ?>
+                    </li>
+                    <li>
+                        <strong>Lingua originale:</strong> <?php echo $dune->original_lenguage ?> ,
+                        <strong>Durata:</strong> <?php echo $dune->duration ?> minuti
+                    </li>
+                    <li><strong>Voto:</strong>
+                        <?php echo $dune->votes ?>
+                        / <strong>Consigliato?</strong>
+                        <?php echo $dune->getRecommendation($dune->votes) ?>
+                    </li>
+                    <li><strong>Costo:</strong>
+                        <?php echo $dune->production_cost ?> milioni
+                    </li>
+                    <li><strong>Incasso:</strong>
+                        <?php echo $dune->tickets_sold ?> milioni
+                    </li>
+                    <li>
+                        <strong>Ci sarà un sequel?</strong>
+                        <?php echo $dune->getSequel($dune->production_cost, $dune->tickets_sold) ?>
+                    </li>
+                </ul>
+            </div>
+            <!-- Halloween kills -->
+            <div class="movie-card">
+                <!-- Postert -->
+                <div class="poster">
+                    <img src="<?php echo $halloween_kills->poster ?>" alt="<?php echo $halloween_kills->title ?>">
+                </div>
+                <ul>
+                    <li class="title">
+                        <?php echo $halloween_kills->title ?>
+                    </li>
+                    <li>
+                        <strong>Genere:</strong>
+                        <?php echo $halloween_kills->genre ?>
+                        <strong>Anno di uscita:</strong>
+                        <?php echo $halloween_kills->exit_year ?>
+                    </li>
+                    <li><strong>Lingua originale:</strong>
+                        <?php echo $halloween_kills->original_lenguage ?> ,
+                        <strong>Durata:</strong> <?php echo $halloween_kills->duration ?> minuti
+                    </li>
+                    <li><strong>Voto:</strong>
+                        <?php echo $halloween_kills->votes ?>
+                        / <strong>Consigliato?</strong>
+                        <?php echo $halloween_kills->getRecommendation($halloween_kills->votes) ?>
+                    </li>
+                    <li><strong>Costo:</strong>
+                        <?php echo $halloween_kills->production_cost ?> milioni
+                    </li>
+                    <li><strong>Incasso:</strong>
+                        <?php echo $halloween_kills->tickets_sold ?> milioni
+                    </li>
+                    <li>
+                        <strong>Ci sarà un sequel?</strong>
+                        <?php echo $halloween_kills->getSequel($halloween_kills->production_cost, $halloween_kills->tickets_sold) ?>
+                    </li>
+                </ul>
+            </div>
+            <!-- Cats -->
+            <div class="movie-card">
+                <!-- Postert -->
+                <div class="poster">
+                    <img src="<?php echo $cats->poster ?>" alt="<?php echo $cats->title ?>">
+                </div>
+                <ul>
+                    <li class="title">
+                        <?php echo $cats->title ?>
+                    </li>
+                    <li>
+                        <strong>Genere:</strong>
+                        <?php echo $cats->genre ?>
+                        <strong>Anno di uscita:</strong>
+                        <?php echo $cats->exit_year ?>
+                    </li>
+                    <li>
+                        <strong>Lingua originale:</strong>
+                        <?php echo $cats->original_lenguage ?> ,
+                        <strong>Durata:</strong> <?php echo $cats->duration ?> minuti
+                    </li>
+                    <li><strong>Voto:</strong>
+                        <?php echo $cats->votes ?> /
+                        <strong>Consigliato?</strong> <?php echo $cats->getRecommendation($cats->votes) ?>
+                    </li>
+                    <li><strong>Costo:</strong>
+                        <?php echo $cats->production_cost ?> milioni
+                    </li>
+                    <li><strong>Incasso:</strong>
+                        <?php echo $cats->tickets_sold ?> milioni
+                    </li>
+                    <li>
+                        <strong>Ci sarà un sequel?</strong>
+                        <?php echo $cats->getSequel($cats->production_cost, $cats->tickets_sold) ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </main>
 </body>
+
 </html>
--- script/database_movie.php
+++ script/database_movie.php
@@ -13,6 +13,7 @@ class Movie {
     //Milion of dollar
     public $production_cost ;
     public $tickets_sold ;
+    public $sequel;
 
     //*Constructor
     function __construct($title, $poster, $genre, $exit_year, $duration, $votes, $original_lenguage, $production_cost, $tickets_sold) {
@@ -22,7 +23,6 @@ class Movie {
         $this->exit_year = $exit_year;
         $this->duration = $duration;
         $this->votes = $votes;
-        $this->recommended = $recommended;
         $this->original_lenguage = $original_lenguage;
         $this->production_cost = $production_cost;
         $this->tickets_sold = $tickets_sold;
@@ -44,15 +44,35 @@ class Movie {
         }
         return $this->recommendation;
     }
+
+    public function getSequel($production_cost, $tickets_sold) {
+        if ($production_cost > $tickets_sold) {
+            $this->sequel = 'Nessuna probabilità';
+        }
+        else if ($tickets_sold > ($production_cost * 3) ) {
+            $this->sequel = "Assolutamente si! GALLINA DALLE UOVA D'OROOOO!!";
+        }
+        else if ($tickets_sold > ($production_cost * 2) ) {
+            $this->sequel = 'Altissime probabilità';
+        }
+        else if ($tickets_sold > $production_cost) {
+            $this->sequel = 'Buone probabilità';
+        }
+        else {
+            $this->sequel = 'Quasi nessuna probabilità';
+        }
+        return $this->sequel;
+    }
+
 }
 
 //*Instance
 
 $avengers_endgame = new Movie('Avengers: End Game', 'https://www.ilpost.it/wp-content/uploads/2019/07/endgame-record.jpg', 'Sci-fi/Fantasy', 2019, 181, 94, 'En', 500, 2792);
 
-$dune = new Movie('Dune', 'Sci-fi/Adventure', 'https://www.meteoweb.eu/wp-content/uploads/2021/09/DUNE.jpg', 2021, 155, 83, 'En', 165, 390);
+$dune = new Movie('Dune', 'https://www.meteoweb.eu/wp-content/uploads/2021/09/DUNE.jpg', 'Sci-fi/Adventure', 2021, 155, 83, 'En', 165, 390);
 
-$halloween_kills = new Movie('Halloween Kills', 'https://www.supergacinema.it/wp-content/uploads/2021/10/Halloween-Kills.jpg', 'Mystery & thriller/Other', 2021, 105, 41, 'En', 20, 85);
+$halloween_kills = new Movie('Halloween Kills', 'https://www.supergacinema.it/wp-content/uploads/2021/10/Halloween-Kills.jpg', 'Mystery & thriller/Other', 2021, 105, 41, 'En', 41, 80);
 
 $cats = new Movie('Cats', 'https://pad.mymovies.it/filmclub/2018/12/057/locandina.jpg', 'Comedy/Musical', 2019, 110, 19, 'En', 95, 73);
 

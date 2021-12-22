4f98efaa58f3d16de033337689c9275e7dfb5715
DanyMencu <dany69men@gmail.com>
2021-12-22 16:45:27 +0100

    Add css style

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ require_once __DIR__ . '/script/database_movie.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Poracciflix PHP</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./CSS/style.css">
 </head>
 
 <body>
